74d8d786b0b0c666fbcc15c4fa9e22150b7c2260
Swapnil Dixit <swapnildixit515@gmail.com>
2022-08-27 12:09:27 +0530

    Updates

---
+++ precache-manifest.53e4ebff1598d97d1dd5035e6c00ecb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a4745810783d37412929cfdb4ad8f2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3227d96a33a33ef877a4",
+    "url": "/react-gh-pages/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "70444a323c69999e6c2c",
+    "url": "/react-gh-pages/static/js/2.b63e21e5.chunk.js"
+  },
+  {
+    "revision": "3227d96a33a33ef877a4",
+    "url": "/react-gh-pages/static/js/main.30f5d0cf.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
